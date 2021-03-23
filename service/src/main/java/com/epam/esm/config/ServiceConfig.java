f626834231ff6ffab851fa9cb1ae74e3ce7c6e86
Kirill Sviridov <gkttk@mail.ru>
2021-03-23 20:38:33 +0300

    Added converters in ServiceConfig as beans

--- service/src/main/java/com/epam/esm/config/ServiceConfig.java
+++ service/src/main/java/com/epam/esm/config/ServiceConfig.java
@@ -1,9 +1,28 @@
 package com.epam.esm.config;
 
+import com.epam.esm.dto.GiftCertificateDto;
+import com.epam.esm.dto.TagDto;
+import com.epam.esm.entity.GiftCertificate;
+import com.epam.esm.entity.Tag;
+import com.epam.esm.service.converter.Converter;
+import com.epam.esm.service.converter.impl.GiftCertificateConverter;
+import com.epam.esm.service.converter.impl.TagConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = {"com.epam.esm.service"})
 public class ServiceConfig {
+
+    @Bean
+    public Converter<GiftCertificate, GiftCertificateDto> getGiftCertificateConverter() {
+        return new GiftCertificateConverter();
+    }
+
+    @Bean
+    public Converter<Tag, TagDto> getTagConverter() {
+        return new TagConverter();
+    }
+
 }

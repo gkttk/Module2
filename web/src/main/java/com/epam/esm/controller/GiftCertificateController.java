27fb4d16395cc53dc3bf185ebe9dfc17ba12242f
Kirill Sviridov <gkttk@mail.ru>
2021-04-05 16:37:36 +0300

    annotation @Validated on class level was removed

--- web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
+++ web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
@@ -8,15 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.validation.Valid;
@@ -24,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@Validated
 @RequestMapping(path = "/certificates", produces = "application/json")
 public class GiftCertificateController {
 
@@ -64,14 +55,14 @@ public class GiftCertificateController {
 
     @PutMapping("/{id}")
     public ResponseEntity<GiftCertificateDto> updateCertificate(@RequestBody @Validated(UpdateGroup.class) @Valid GiftCertificateDto giftCertificateDto,
-                                                  @PathVariable long id) {
+                                                                @PathVariable long id) {
         GiftCertificateDto updatedDto = giftCertificateService.update(giftCertificateDto, id);
         return ResponseEntity.ok(updatedDto);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<GiftCertificateDto> patchCertificate(@RequestBody @Validated(PatchGroup.class) @Valid GiftCertificateDto giftCertificateDto,
-                                                 @PathVariable long id) {
+                                                               @PathVariable long id) {
         GiftCertificateDto patchedDto = giftCertificateService.patch(giftCertificateDto, id);
         return ResponseEntity.ok(patchedDto);
     }

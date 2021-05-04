8e3bcdd30b7fd57d91ff939fe959926b9b7fcae3
Kirill Sviridov <gkttk@mail.ru>
2021-05-04 09:57:47 +0300

    +GiftCertificateControllerTest

--- web/src/test/java/com/epam/esm/controller/GiftCertificateControllerTest.java
+++ web/src/test/java/com/epam/esm/controller/GiftCertificateControllerTest.java
@@ -1,22 +1,18 @@
-/*
 package com.epam.esm.controller;
 
 
-import com.epam.esm.ApplicationRunner;
+import com.epam.esm.assemblers.GiftCertificateModelAssembler;
 import com.epam.esm.dto.GiftCertificateDto;
 import com.epam.esm.dto.TagDto;
 import com.epam.esm.service.GiftCertificateService;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.context.request.WebRequest;
 
 import java.math.BigDecimal;
@@ -29,11 +25,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-//\test\gifts.web
-
 @ExtendWith(MockitoExtension.class)
 public class GiftCertificateControllerTest {
-
+    @Mock
+    private GiftCertificateModelAssembler assemblerMock;
     @Mock
     private GiftCertificateService serviceMock;
     @Mock
@@ -42,6 +37,9 @@ public class GiftCertificateControllerTest {
 
     @InjectMocks
     private GiftCertificateController giftCertificateController;
+
+    private static final int TEST_LIMIT = 5;
+    private static final int TEST_OFFSET = 0;
 
     private static GiftCertificateDto defaultCertDto;
     private static GiftCertificateDto testDto;
@@ -73,39 +71,38 @@ public class GiftCertificateControllerTest {
     }
 
     @Test
-    public void dos(){}
-
-
-   */
-/* @Test
     public void testGetAllForQuery_ReturnHttpStatusOkWithDtoList() {
         //given
-
         Map<String, String[]> paramMap = new HashMap<>();
         List<GiftCertificateDto> listDto = Arrays.asList(defaultCertDto, defaultCertDto);
         when(webRequestMock.getParameterMap()).thenReturn(paramMap);
-        when(serviceMock.findAllForQuery(paramMap, , )).thenReturn(listDto);
+        when(serviceMock.findAllForQuery(paramMap, TEST_LIMIT, TEST_OFFSET)).thenReturn(listDto);
+        when(assemblerMock.toCollectionModel(listDto, TEST_OFFSET)).thenReturn(CollectionModel.of(listDto));
+        ResponseEntity<CollectionModel<GiftCertificateDto>> expectedResult = ResponseEntity.ok(CollectionModel.of(listDto));
 
-        ResponseEntity<List<GiftCertificateDto>> expected = ResponseEntity.ok(listDto);
         //when
-        ResponseEntity<List<GiftCertificateDto>> result = giftCertificateController.getAllForQuery(webRequestMock);
+        ResponseEntity<CollectionModel<GiftCertificateDto>> result = giftCertificateController.getAllForQuery(webRequestMock, TEST_LIMIT, TEST_OFFSET);
         //then
-        assertEquals(result, expected);
+        assertEquals(result, expectedResult);
         verify(webRequestMock).getParameterMap();
-        verify(serviceMock).findAllForQuery(paramMap, , );
+        verify(serviceMock).findAllForQuery(paramMap, TEST_LIMIT, TEST_OFFSET);
+        verify(assemblerMock).toCollectionModel(listDto, TEST_OFFSET);
     }
+
 
     @Test
     public void testGetById_ReturnHttpStatusOkWithDto() {
         //given
         Long certId = defaultCertDto.getId();
         when(serviceMock.findById(certId)).thenReturn(defaultCertDto);
+        when(assemblerMock.toModel(defaultCertDto)).thenReturn(defaultCertDto);
         ResponseEntity<GiftCertificateDto> expected = ResponseEntity.ok(defaultCertDto);
         //when
         ResponseEntity<GiftCertificateDto> result = giftCertificateController.getById(certId);
         //then
         assertEquals(result, expected);
         verify(serviceMock).findById(certId);
+        verify(assemblerMock).toModel(defaultCertDto);
     }
 
     @Test
@@ -121,16 +118,17 @@ public class GiftCertificateControllerTest {
     }
 
     @Test
-    @Disabled
     public void testCreateCertificate_ThereAreNoValidationErrors_ReturnHttpStatusCreated() {
         //given
         when(serviceMock.save(testDto)).thenReturn(defaultCertDto);
-        ResponseEntity<GiftCertificateDto> expected = new ResponseEntity<>(defaultCertDto, HttpStatus.CREATED);
+        when(assemblerMock.toModel(defaultCertDto)).thenReturn(defaultCertDto);
+        ResponseEntity<GiftCertificateDto> expected = ResponseEntity.ok(defaultCertDto);
         //when
         ResponseEntity<GiftCertificateDto> result = giftCertificateController.createCertificate(testDto);
         //then
         assertEquals(result, expected);
         verify(serviceMock).save(testDto);
+        verify(assemblerMock).toModel(defaultCertDto);
     }
 
     @Test
@@ -138,27 +136,31 @@ public class GiftCertificateControllerTest {
         //given
         Long certId = defaultCertDto.getId();
         when(serviceMock.patch(testDto, certId)).thenReturn(defaultCertDto);
+        when(assemblerMock.toModel(defaultCertDto)).thenReturn(defaultCertDto);
         ResponseEntity<GiftCertificateDto> expected = ResponseEntity.ok(defaultCertDto);
         //when
         ResponseEntity<GiftCertificateDto> result = giftCertificateController.patchCertificate(testDto, certId);
         //then
         assertEquals(result, expected);
         verify(serviceMock).patch(testDto, certId);
+        verify(assemblerMock).toModel(defaultCertDto);
     }
+
 
     @Test
     public void testUpdateCertificate_ThereAreNoValidationErrors_ReturnHttpStatusNoContent() {
         //given
         Long certId = defaultCertDto.getId();
         when(serviceMock.update(testDto, certId)).thenReturn(defaultCertDto);
+        when(assemblerMock.toModel(defaultCertDto)).thenReturn(defaultCertDto);
         ResponseEntity<GiftCertificateDto> expected = ResponseEntity.ok(defaultCertDto);
         //when
         ResponseEntity<GiftCertificateDto> result = giftCertificateController.updateCertificate(testDto, certId);
         //then
         assertEquals(result, expected);
         verify(serviceMock).update(testDto, certId);
+        verify(assemblerMock).toModel(defaultCertDto);
     }
-*//*
+
 
 }
-*/

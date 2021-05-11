caae742c92b99a13a5417925c3083cdafaa0d2db
Kirill Sviridov <gkttk@mail.ru>
2021-05-11 12:52:32 +0300

    removed annotation EnableJpaAuditing from DaoConfig class

--- dao/src/main/java/com/epam/esm/config/DaoConfig.java
+++ dao/src/main/java/com/epam/esm/config/DaoConfig.java
@@ -5,13 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.epam.esm")
-@EnableJpaAuditing
 public class DaoConfig {
 
     @Bean

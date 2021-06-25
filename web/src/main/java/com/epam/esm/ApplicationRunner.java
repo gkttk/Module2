21b2eb0defc3c49af1de0a9558c593b8af50ec82
Kirill Sviridov <gkttk@mail.ru>
2021-06-25 12:34:40 +0300

    ApplicationRunner extends SpringBootServletInitializer

--- web/src/main/java/com/epam/esm/ApplicationRunner.java
+++ web/src/main/java/com/epam/esm/ApplicationRunner.java
@@ -2,11 +2,18 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationRunner {
+public class ApplicationRunner extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApplicationRunner.class);
     }
 }
 
